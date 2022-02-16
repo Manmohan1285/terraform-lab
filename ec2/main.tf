2359da7182ee7e412c8ce3d557be0df683faa486
Manmohan1285 <99814351+Manmohan1285@users.noreply.github.com>
2022-02-16 19:33:31 +0530

    done

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,23 +1,31 @@
+terraform {
+  backend "remote" {
+    organization = "mmh"
+
+    workspaces {
+      name = "sample"
+    }
+  }
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.12.9"
+}
 
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "ec2" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "app_server" {
+  ami           = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "var.instance_name"
+    Name = var.instance_name
   }
-}
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.mmh.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.mmh.public_ip
 }
